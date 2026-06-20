cf5c5832a0d25c2c3af4164388a98063df85c340
alby-lab <go2albert@gmail.com>
2026-06-20 13:58:01 -0700

    feat(account-service): docker file modified and removed unnecessry imports from test class

--- account-service/src/test/java/com/eventledger/account/controller/AccountControllerIntegrationTest.java
+++ account-service/src/test/java/com/eventledger/account/controller/AccountControllerIntegrationTest.java
@@ -1,7 +1,10 @@
 package com.eventledger.account.controller;
 
-import com.eventledger.account.repository.AccountRepository;
-import com.eventledger.account.repository.TransactionRepository;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,9 +14,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import com.eventledger.account.repository.AccountRepository;
+import com.eventledger.account.repository.TransactionRepository;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
